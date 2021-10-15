c6974b3aa4615836b71fd5074ad4273fbbda85f6
TylerDurden75 <raytibo.j@gmail.com>
2021-10-15 16:57:48 +0200

    setup express

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from middleware');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+// Server start
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on ${port}...`);
+});
