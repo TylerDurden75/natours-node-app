64c26f3efa95ebd0c103191bdd80bf32e6e9f304
TylerDurden75 <raytibo.j@gmail.com>
2021-10-18 20:04:25 +0200

    Pagination method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -48,13 +48,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // Execute query
     const tours = await query;
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
 
     // Send repsonse
     res.status(200).json({
