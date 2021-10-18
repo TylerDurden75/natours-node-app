1e71a91cd11f327c29a5869a1f17966a5a1b1a32
TylerDurden75 <raytibo.j@gmail.com>
2021-10-18 18:37:23 +0200

    sorting filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,9 +29,16 @@ exports.getAllTours = async (req, res) => {
     // 2) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // Execute query
     const tours = await query;
