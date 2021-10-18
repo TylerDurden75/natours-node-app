9b27144324ee22ae88ff64b32addd08a854d1029
TylerDurden75 <raytibo.j@gmail.com>
2021-10-18 14:57:57 +0200

    DB hosted with MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful !'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
