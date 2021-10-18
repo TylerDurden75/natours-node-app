49fc0f33f3c2ec62f93ce998e4bce7d50648fa14
TylerDurden75 <raytibo.j@gmail.com>
2021-10-18 17:43:45 +0200

    fixing path warning eslint

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
+const Tour = require('../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
