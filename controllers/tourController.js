0870a46015a6f285b0212678e6fc7d33b6d65191
TylerDurden75 <raytibo.j@gmail.com>
2021-10-18 15:53:00 +0200

    updating createDoc with moongose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,15 +6,15 @@ const Tour = require('./../models/tourModel');
 
 //Middleware
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(404).json({
+//       status: 'failed',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
 
 //Routes Handlers
 exports.getAllTours = (req, res) => {
@@ -42,13 +42,22 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent !',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
