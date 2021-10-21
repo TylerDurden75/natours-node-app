0321b99e0b640cecf0cae6d36d0d1fcce12d7f79
TylerDurden75 <raytibo.j@gmail.com>
2021-10-21 17:51:12 +0200

    Unhandled rejections server

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ mongoose
   .then(() => console.log('DB connection successful !'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection ! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
