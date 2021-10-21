00b9a9e8a1266c16783ceebe049c64c697d11657
TylerDurden75 <raytibo.j@gmail.com>
2021-10-21 15:18:11 +0200

    Error handling Pros VS Dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //  Operational error => send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other error: no leak error to client
+  } else {
+    // 1) log error
+    console.error('ERROR !', err);
+    //2) send message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong !',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
