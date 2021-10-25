1664c20e63386e30f76625ac7600bf0b6d946419
TylerDurden75 <raytibo.j@gmail.com>
2021-10-25 12:37:35 +0200

    reset token v1

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,3 +105,19 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  //1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address.', 404));
+  }
+
+  //2) Generate the random reset
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  //3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
