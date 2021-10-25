ca90fd5d806980a2dca3824b7f46eeb71bb20f7e
TylerDurden75 <raytibo.j@gmail.com>
2021-10-25 18:47:39 +0200

    Populating guides

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,7 +45,6 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
-
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
   }
