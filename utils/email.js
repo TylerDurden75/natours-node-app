5d402dc3c80db2ea54e360564e81bc3dd695fa1c
TylerDurden75 <raytibo.j@gmail.com>
2021-10-25 14:05:18 +0200

    Forget pass and setting new one

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
 
   //2) Define email options
   const mailOptions = {
-    from: 'testmailer <test@mailer.com>',
+    from: 'testmailer <titi@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
