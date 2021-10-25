753eba7b610140839a34e75457976d67911e1edd
TylerDurden75 <raytibo.j@gmail.com>
2021-10-25 13:23:13 +0200

    nodemailer integration

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  //1) Create a tranporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  //2) Define email options
+  const mailOptions = {
+    from: 'testmailer <test@mailer.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  //3) Send email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
