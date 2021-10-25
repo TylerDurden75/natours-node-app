f7c158725def8401e116f0b73a63e0a629628e55
TylerDurden75 <raytibo.j@gmail.com>
2021-10-25 16:12:55 +0200

    rate limiting for preventing attacks Ddos or BF

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARE
+// GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Middleware for preventing BF or Ddos
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in 1 hour!',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
