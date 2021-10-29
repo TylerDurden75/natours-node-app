035416dfc726e06b07cfcde5d690b8df58b08574
TylerDurden75 <raytibo.j@gmail.com>
2021-10-29 15:23:09 +0200

    Login integration + MapBox bug to fix

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -15,7 +15,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
@@ -26,3 +26,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -1,4 +1,48 @@
 /*eslint-disable */
-
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/rayti/ckvccf9dm2ctb15o8wai2m5l6',
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745],
+  // zoom: 6,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add Marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // App Pop-Up
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
