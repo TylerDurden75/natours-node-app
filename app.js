a73f5566e6590357ef5a2134b78662e96bb938b9
TylerDurden75 <raytibo.j@gmail.com>
2021-10-29 16:23:29 +0200

    Login integration + must fix bug on cookie

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanatize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -42,6 +43,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanatization against NoSql query injection
 app.use(mongoSanatize());
@@ -66,7 +68,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
+// const authController = require('../controllers/authController');
 
 const router = express.Router();
 
