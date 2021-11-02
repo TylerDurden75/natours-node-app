1d3771c5ef0aa0ff697076233ee128c49e7e011b
TylerDurden75 <raytibo.j@gmail.com>
2021-11-02 20:51:23 +0100

    Pass fix bug

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9245,7 +9245,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53372" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49738" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
