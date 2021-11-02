47e27bae0e9c01e3c37c77c6ee87a9e62b8fdd6f
TylerDurden75 <raytibo.j@gmail.com>
2021-11-02 18:21:15 +0100

    Create Email Handler based on Sendgrid

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,53 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlTotText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //1) Create a tranporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Tyler Durden <${process.env.EMAIL_FROM}>`;
+  }
 
-  //2) Define email options
-  const mailOptions = {
-    from: 'testmailer <titi@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        password: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  //3) Send email
-  await transporter.sendMail(mailOptions);
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on pug
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlTotText.fromString(html),
+      // html:
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the NodeTours Family!');
+  }
 };
-
-module.exports = sendEmail;
