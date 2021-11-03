131a68c3ad33a742ee40d6adda4692b15f36eacf
TylerDurden75 <raytibo.j@gmail.com>
2021-11-03 13:04:57 +0100

    refactoring

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,9 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
-const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
